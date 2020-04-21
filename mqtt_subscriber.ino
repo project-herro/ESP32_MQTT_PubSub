cd1315470b71ca528aa3e63f34a0f426684c22f1
75u2u <75u2uuy4@gmail.com>
2020-04-21 22:26:44 +0900

    Create mqtt_subscriber.ino

---
+++ mqtt_subscriber.ino
@@ -0,0 +1,59 @@
+
+#include <WiFi.h>
+#include <MQTTClient.h>
+
+// SSID, PASSWORD
+const char ssid[] = "XXX";
+const char pass[] = "YYY";
+
+WiFiClient net;
+MQTTClient client;
+
+unsigned long lastMillis = 0;
+
+void connect() {
+  Serial.print("checking wifi...");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(1000);
+  }
+
+  // shiftr "key", "Secret"
+  Serial.print("\nconnecting...");
+  while (!client.connect("ESP32_sub", "6ca7a569", "91d9b265f7ac5cab")) {
+    Serial.print(".");
+    delay(1000);
+  }
+
+  Serial.println("\nconnected!");
+  client.subscribe("/hello");
+}
+
+
+void messageReceived(String &topic, String &payload) {
+  Serial.println("incoming: " + topic + " - " + payload);
+  delay(10);
+}
+
+
+void setup() {
+  Serial.begin(115200);
+  WiFi.begin(ssid, pass);
+  
+  // broker
+  client.begin("broker.shiftr.io", net);
+  client.onMessage(messageReceived);
+
+  connect();
+}
+
+
+void loop() {
+  client.loop();
+  delay(10);
+
+  if (!client.connected()) {
+    connect();
+  }
+  client.onMessage(messageReceived);
+}
